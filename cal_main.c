1c8133e222381042a9c97c02742c16cf790ba92e
Jong-Chan Kim <jongchank@gmail.com>
2022-11-11 14:14:28 +0900

    Add decoration to calculator

--- cal_main.c
+++ cal_main.c
@@ -7,10 +7,12 @@ int main(int argc, char *argv[])
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
 
+    printf("===============\n");
     printf("%d + %d => %d\n", a, b, add(a, b));
     printf("%d - %d => %d\n", a, b, sub(a, b));
     printf("%d * %d => %d\n", a, b, mul(a, b));
     printf("%d / %d => %d\n", a, b, dur(a, b));
+    printf("===============\n");
 
     return 0;
 }
